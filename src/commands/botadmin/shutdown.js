135bb577d21e1e98958b9c2cd419116771b53770
GaboTheCoder <72574725+GaboTheCoder@users.noreply.github.com>
2021-07-12 14:34:34 -0400

    Added the shutdown command

---
+++ src/commands/botadmin/shutdown.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-unused-vars */
+const { Command, CommandoMessage } = require("discord.js-commando");
+const { MessageEmbed } = require("discord.js");
+const { oneLine } = require("common-tags");
+
+module.exports = class RestartCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: "shutdown",
+      memberName: "shutdown",
+      group: "botadmin",
+      description: "none of your business",
+      clientPermissions: [],
+      argsType: "multiple",
+      ownerOnly: true
+    });
+  }
+
+  /**
+   * @param {CommandoMessage} message
+   */
+
+  run(message) {
+    const RestartEmbed = new MessageEmbed()
+      .setTitle("Are you sure you want to shutdown?")
+      .setThumbnail(this.client.user.avatarURL())
+      .setFooter(
+        `${message.author.tag} (${message.author.id})`,
+        message.author.avatarURL()
+      )
+      .setTimestamp()
+      .setDescription(
+        oneLine`
+        This will shut down the bot completely. 
+        Do you want to continue? This message will be ignored after 30 seconds.`
+      );
+
+    message.channel.send(RestartEmbed).then(async (msg) => {
+      msg.react("✅").then(() => msg.react("❌"));
+
+      const filter = (reaction, user) => {
+        return (
+          ["✅", "❌"].includes(reaction.emoji.name) &&
+          user.id === message.author.id
+        );
+      };
+
+      msg
+        .awaitReactions(filter, { max: 1, time: 30000, errors: ["time"] })
+        .then(async (collected) => {
+          const reaction = collected.first();
+
+          if (reaction.emoji.name === "✅") {
+            message.channel.send("Shutting down...").then(() => {
+              this.client.destroy();
+            });
+          } else {
+            message.channel.send("Action canceled.");
+          }
+        })
+        .catch((collected) => {
+          message.channel.send("Exceeded 30 second threshold.");
+        });
+    });
+  }
+};
