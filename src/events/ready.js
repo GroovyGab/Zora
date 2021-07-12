20473d409b0c72d643745fc60f471e819c84f991
GaboTheCoder <72574725+GaboTheCoder@users.noreply.github.com>
2021-07-12 14:34:52 -0400

    Remove some bullshit XDDD

--- src/events/ready.js
+++ src/events/ready.js
@@ -8,7 +8,6 @@ module.exports = class extends Event {
   }
 
   run() {
-    this.client.user.setActivity("el iñaki es gay");
     console.log(
       `Client ready; logged in as ${this.client.user.username}#${this.client.user.discriminator} (${this.client.user.id})`
     );
