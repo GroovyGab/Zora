780cab2921168a30744f3b6e5f00415230829ca1
GaboTheCoder <72574725+GaboTheCoder@users.noreply.github.com>
2021-07-13 10:36:41 -0400

    Add MongoDB Connection

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,5 @@
 const Event = require("../structures/Event");
+const mongo = require("../database/mongodb/mongo");
 
 module.exports = class extends Event {
   constructor(...args) {
@@ -7,7 +8,17 @@ module.exports = class extends Event {
     });
   }
 
-  run() {
+  async run() {
+    await mongo().then((mongoose) => {
+      try {
+        console.log("[MongoDB] Connected to database.");
+      } catch (err) {
+        console.log("There was an error with MongoDb", err);
+      } finally {
+        mongoose.connection.close();
+      }
+    });
+
     console.log(
       `Client ready; logged in as ${this.client.user.username}#${this.client.user.discriminator} (${this.client.user.id})`
     );
